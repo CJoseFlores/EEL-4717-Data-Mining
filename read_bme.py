b04314ce92a19070f884a923b80fd37f1adc2fb2
Carlos Flores <cflor083@fiu.edu>
2017-09-29 16:04:37 +0000

    Grabs timestamp in UTC.

--- read_bme.py
+++ read_bme.py
@@ -1,4 +1,5 @@
 from bme_280.bme280 import *
+import datetime
 import urllib2
 import time
 import json
@@ -13,10 +14,18 @@ while(True):
     # Grab a tuple that returns temp, pressure and humidity.
     temp, pressure, humidity = readBME280All()
 
-    # Parsing values into json template.
+    # Grab current time, and convert to required format.
+    utc_time = datetime.datetime.utcnow()
+    time_stamp = str(utc_time.strftime("%m.%d.%Y %I:%M %p")) 
+
+    # Parsing values into the json template.
     sensor_post['values'][0]['value'] = temp
     sensor_post['values'][1]['value'] = pressure
     sensor_post['values'][2]['value'] = humidity
+
+    # Parsing timestamps into the json template.
+    for sensor_dict in sensor_post['values']:
+        sensor_dict['timeStamp'] = time_stamp
 
     # Printing out json for verification.
     print json.dumps(sensor_post, indent=2)
