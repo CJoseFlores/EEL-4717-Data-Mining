53b2f679abeb949e76d398da491d9ea519508071
Carlos Flores <cflor083@fiu.edu>
2017-09-30 14:43:45 +0000

    Removed lines that were not needed.

--- read_bme.py
+++ read_bme.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 from datetime import datetime
 from bme_280.bme280 import *
 import pendulum
-import requests
+import requests 
 import time
 import json
 import ssl
@@ -10,33 +10,11 @@ import ssl
 # Loading json template to send as a POST request to MongoDB server.
 f = open('bme_template.json')
 sensor_post = json.loads(f.read(), object_pairs_hook=OrderedDict)
-#sensor_post = json.loads(f.read())
 f.close()
 
 # URL and Header used for HTTP PUT requests.
 url = "https://10.109.143.88:8443/sendsensorvalue/"
 headers = {"content-type" : "application/json"}
-#data = {
-#     "pantherId":5160328,
-#     "values":[
-#         {
-#             "variableName":"Temperature",
-#             "timeStamp":"09.25.2017 2:37 AM",
-#             "value": 23.45
-#         },
-#         {
-#             "variableName":"Pressure",
-#             "timeStamp":"09.25.2017 2:37 AM",
-#             "value": 1012.087435
-#         },
-#         {
-#             "variableName":"Humidity",
-#             "timeStamp":"09.25.2017 2:37 AM",
-#             "value": 49.6455102
-#         }
-#     ]
-#}
-#   
 
 # Continue to read and display temperature, pressure and humidity values to console.
 while(True):
@@ -58,7 +36,6 @@ while(True):
         sensor_dict['timeStamp'] = time_stamp
 
     # Printing out json for verification.
-    print sensor_post
     print json.dumps(sensor_post, indent=2)
     f = open('sample_output.json', 'w')
     f.write(json.dumps(sensor_post, indent=2))
@@ -67,15 +44,11 @@ while(True):
     # Sending out formatted json to MongoDB server, and print response.
     try:
         req = requests.put(url, headers=headers, data=json.dumps(sensor_post), verify=False)
-
         print req.text
     except requests.exceptions.ConnectionError as ce:
         print "------------------------------------------Server is unreachable.------------------------------------------"
         print ce
         print "----------------------------------------------------------------------------------------------------------"
-
-#    req = requests.put(url, headers=headers, data=str(sensor_post), verify=False)
-#    print req
 
     # Print out the values from sample.
     print "Temperature : " + str(temp) + " C"
